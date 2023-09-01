8364e281bdc7e3c88a3da93147ea93654faf7c48
UMURAGWA <ishimwehertier4@gmail.com>
2023-09-01 16:12:22 -0700

    This is the first

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,9 @@
-#include &lt;stdio.h&gt;
-#include &quot;main.h&quot;
-/**
- * main - prints the name of the program
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: Always 0 (Success)
- */
-int main(int argc __attribute__((unused)), char *argv[])
-{
-        printf(&quot;%s\n&quot;, *argv);
-        return (0);
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    (void)argc; /* Suppress unused variable warning*/
+
+    printf("%s\n", argv[0]);
+
+    return 0;
 }
