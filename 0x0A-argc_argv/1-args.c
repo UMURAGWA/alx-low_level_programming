b5ea31c19c96b023e1c0a1a0af9559a03ac6348e
UMURAGWA <ishimwehertier4@gmail.com>
2023-09-01 16:28:35 -0700

    The last one

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
+#include "main.h"
 
-int main(int argc, char *argv[]) {
-    (void)argv; /* Suppress unused variable warning*/
+/**
+ * main - print the number of arguments passed to the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
 
-    printf("%d\n", argc - 1);
+int main(int argc, char *argv[])
+{
+(void) argv; /*Ignore argv*/
+	printf("%d\n", argc - 1);
 
-    return 0;
+	return (0);
 }
